0f8e96c2006a1e29bc2acdcbddf115798a1ac458
leeorn <padiii@gmail.com>
2018-09-06 00:03:21 -0700

    Methods to add to front or end

--- src/Leeor/Main.java
+++ src/Leeor/Main.java
@@ -8,13 +8,16 @@ public class Main {
 
         Employee one = new Employee ("Amit", "Aola", 6);
         Employee two = new Employee ("Bill", "Bob", 12);
-        Employee three = new Employee ("Coco", "Chill", 27);
-        Employee four = new Employee ("Didi", "Dobe", 33);
+        Employee three = new Employee ("Coco", "Chill", 23);
+        Employee four = new Employee ("Didi", "Dobe", 34);
+        Employee five = new Employee("Ed","Eddie",45);
 
         employee.addToFront(one);
         employee.addToFront(two);
         employee.addToFront(three);
         employee.addToFront(four);
+        employee.addToEnd(five);
+
 
         employee.printList();
 //        System.out.println(employee.head.getPrev());
